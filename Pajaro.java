42e0b22fae886785d87240f08aa40a69f80a7ea4
Desarrollador Java Script <jhonson123456789castro@gmail.com>
2026-03-02 19:29:35 -0500

    Add Pajaro class extending Animal

---
+++ Pajaro.java
@@ -0,0 +1,17 @@
+
+public class Pajaro extends Animal {
+
+    public Pajaro(String nombre, int edad) {
+        super(nombre, edad);
+    }
+
+
+    public void hacerSonido() {
+        System.out.println("Pío pío");
+    }
+
+
+    public void moverse() {
+        System.out.println("El pájaro vuela");
+    }
+}
